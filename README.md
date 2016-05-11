9a07c7c89b121f58ee5949c904a432c7c953b7e8
Edouard de Labareyre <edouard@capitainetrain.com>
2016-05-11 10:24:35 +0200

    First version of the script

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # lat-long-launcher
-A ruby script to open a map based on any latitude-longitude you copy-paste brutally
+A ruby script to open a map based on any latitude-longitude you copy-paste brutally.
+
+:warning: This is currently a work in progress, the script does not recognize many patterns yet.
+
+## How to use it
+
+You'll need ruby and bundler installed on your system.
+
+```bash
+git clone https://github.com/inouire/lat-long-launcher.git
+cd lat-long-launcher
+bundle install
+ruby display_on_map.rb your raw string
+```
+
+## Some examples of what the script is able to recognize
+
+- `Lat: 50,83653 Long: 4,33904`
+- `foo 50.83/4,33904 bar`
+- `4.33904  -- 50.83 `
---
+++ display_on_map.rb
@@ -0,0 +1,37 @@
+require 'launchy'
+
+# TODO make it configurable through a file
+LAT_RANGE  = -30.0..30.0
+LONG_RANGE = 30..60
+
+def clean_args(argv)
+   argv.join(" ").gsub(",", ".").downcase
+end
+
+def extract_lat_long(input)
+  coords = input.scan(/-?\d{1,2}\.\d+/i).map(&:to_f)
+
+  raise ArgumentError, "Found #{coords.size} coordinates, expected 2" if coords.size != 2
+
+  if LAT_RANGE === coords[0] && LONG_RANGE === coords[1]
+    [coords[0], coords[1]]
+  elsif LONG_RANGE === coords[0] && LAT_RANGE === coords[1]
+    [coords[0], coords[1]]
+  else
+    raise ArgumentError, "Found coordinates #{coords} not in range (latitude: #{LAT_RANGE}, longitude: #{LONG_RANGE})"
+  end
+end
+
+def open_osm_on(latitude, longitude)
+  osm_url  = "http://www.openstreetmap.org"
+  full_url = "#{osm_url}/?mlat=#{latitude}&mlon=#{longitude}#map=15/#{latitude}/#{longitude}&layers=Q"
+  Launchy.open(full_url)
+end
+
+full_input = clean_args(ARGV)
+
+puts "Trying to extract valid lat long from '#{full_input}'"
+lat, long = extract_lat_long(full_input)
+
+puts "Latitude and longitude found (#{lat}, #{long}), opening OpenStreetMaps in a new browser tab"
+open_osm_on(lat, long)
